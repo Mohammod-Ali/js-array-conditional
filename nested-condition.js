6422b08b59b0cccacda83295fd86a8b60ece0479
Mohammod Ali <mohammodali651@gmail.com>
2022-08-01 22:23:27 +0600

    nested condition added

---
+++ nested-condition.js
@@ -0,0 +1,26 @@
+var cakePrice = 50;
+var danishPrice = 25;
+var toastBiscuitPrice = 10;
+var myBudget = 40;
+var packWell = false;
+
+if (cakePrice < myBudget){
+    if(packWell == true){
+        console.log('cake khamu')
+    }
+}
+else {
+    console.log('mosha masi ordhek kheye felse');
+}
+// if (cakePrice < myBudget) {
+//     console.log('cake khamu');
+// }
+// else if ( danishPrice < myBudget) {
+//     console.log('danish diye cha khamu');
+// }
+// else if ( toastBiscuitPrice < myBudget) {
+//     console.log('toast bisciot khamu');
+// }
+// else {
+//     console.log('batasha diye cha khamu');
+// }
---
+++ push-pop.js
@@ -0,0 +1,6 @@
+var lastbanch = ['kalam', 'balam', 'salam'];
+lastbanch.push('jalam');
+lastbanch.pop();
+lastbanch.unshift('jullu');
+lastbanch.shift();
+console.log(lastbanch)
