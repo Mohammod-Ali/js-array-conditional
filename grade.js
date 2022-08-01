32e6c46e416d489edb65ff32446e838468191bb5
Mohammod Ali <mohammodali651@gmail.com>
2022-08-01 20:13:33 +0600

    first commit

---
+++ grade.js
@@ -0,0 +1,18 @@
+// Practice Problem 2
+var score = 79;
+
+if(80 <= score){
+    console.log('You got A grade')
+}
+else if(60 <= score){
+    console.log('You got B Grade')
+}
+else if(50 <= score){
+    console.log('You got C Grade')
+}
+else if(40 <= score){
+    console.log('You got D Grade')
+}
+else{
+    console.log('FFFFFF')
+}
